056c8f09b9c0f7bc52da9022c04cd33b8e07fdad
JerichElano <jerichelano@gmail.com>
2023-11-01 19:53:42 +0800

    section modification
    I made it in sections so that we can also add other CPU scheduling

--- third.py
+++ third.py
@@ -35,31 +35,55 @@ def main():
     arrival_times = []
     burst_times = []
     tupol = []
-    j = int(input("Enter number of processes: "))
-    ordinal = ordinal_suffixes(j)
+    print("=====================================================")
+    print("Welcome to the Process Scheduling Simulator")
+    print("=====================================================")
+    
+    valid = False
+    while valid == False:
+        process = input("What process scheduling algorithm would you like to use? (FCFS, SJF, RR): ")
+        process = process.upper()
+        if process in ["FCFS", "SJF", "RR"]:
+            valid = True
+        else:
+            print("Invalid input, please try again.")
+
+    print("=====================================================")        
+
+    if process == "FCFS":
+        j = int(input("Enter number of processes: "))
+        ordinal = ordinal_suffixes(j)
+
+        for i in range(j):
+            job = input(f"Enter {ordinal[i+1]} job name: ")
+            arrival = int(input(f"Enter arrival time for the job {job}: "))
+            burst = int(input(f"Enter burst time for {job}: "))
+            tupol.append((job, arrival, burst))
+            
+        tupol.sort(key = lambda x: x[1]) #for sorting our array "tupol"
+
+        jobs = [t[0] for t in tupol] #this sellect items from the tuple and put it inside an array
+        arrival_times = [t[1] for t in tupol]
+        burst_times = [t[2] for t in tupol]
+
+        turnaround_times, waiting_times = calculate_turnaround_waiting_time(jobs, arrival_times, burst_times)
+
+    elif process == "SJF":
+        j = int(input("Enter number of processes: "))
+        ordinal = ordinal_suffixes(j)
+
+        for i in range(j):
+            job = input(f"Enter {ordinal[i+1]} job name: ")
+            arrival = int(input(f"Enter arrival time for the job {job}: "))
+            burst = int(input(f"Enter burst time for {job}: "))
+            tupol.append((job, arrival, burst))
+            
+        tupol.sort(key = lambda x: x[2])
 
 
+    print("\n   Job\t|   AT\t|   BT\t|   TAT \t|   WT")
     for i in range(j):
-        job = input(f"Enter {ordinal[i+1]} job name: ")
-        arrival = int(input(f"Enter arrival time for the job {job}: "))
-        burst = int(input(f"Enter burst time for {job}: "))
-        tupol.append((job, arrival, burst))
-
-    tupol.sort(key = lambda x: x[1]) #for sorting our array "tupol"
-    print(tupol)
-
-    jobs = [t[0] for t in tupol] #this sellect items from the tuple and put it inside an array
-    arrival_times = [t[1] for t in tupol]
-    burst_times = [t[2] for t in tupol]
-
-    print(arrival_times)
-    print(burst_times)
-
-    turnaround_times, waiting_times = calculate_turnaround_waiting_time(jobs, arrival_times, burst_times)
-
-    print("\nJob\tArrival Time\tBurst Time\tTurnaround Time\tWaiting Time")
-    for i in range(j):
-        print(f"{jobs[i]}\t{arrival_times[i]}\t\t{burst_times[i]}\t\t{turnaround_times[i]}\t\t{waiting_times[i]}")
+        print(f"    {jobs[i]}\t|   {arrival_times[i]}\t|   {burst_times[i]}\t|   {turnaround_times[i]}\t|   {waiting_times[i]}")
 
 
 main()
